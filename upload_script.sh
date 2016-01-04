3b0864c1742e0f90519ba4a10fcc9c666cac6ccd
Junqiao Wang <junqiao.wang@hulu.com>
2016-01-04 04:34:17 -0800

    .:w

--- upload_script.sh
+++ upload_script.sh
@@ -6,6 +6,6 @@ file=$2
 [ -z "$file" ] && echo "Please specify a file path." && exit 0
 [ ! -f "$file" ] && echo "Specified file is not exist." && exit 0
 creator=$(curl -sb -H "Accept: application/json" "http://api-compare-tool.server.hulu.com/api/tasks/get_creator?id=$taskId")
-[ -z "$creator" ] && echo "Task is not exist with specified id." && exit 0
+# [ -z "$creator" ] && echo "Task is not exist with specified id." && exit 0
 curl -i -X PUT -T "$file" -L "http://10.16.97.213:50070/webhdfs/v1/user/search/$taskId.txt?op=CREATE&user.name=search&overwrite=true&blocksize=1048576"
 
